6cf62c18d7757e57aa9e587c407561b4380f01d2
liuzhixin <liuzhixin@huorong.cn>
2023-08-02 10:13:33 +0800

    代码提交总

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,25 +1,25 @@
-/**
- * @author lzx
- * @date 2023/2/21 14:47
- * @description: TODO
- */
-public class Test {
-    public static void main(String[] args) {
-
-    }
-
-    @org.junit.jupiter.api.Test
-    public void agg(){
-
-       int a = -2147483648;
-       StringBuilder s1 = new StringBuilder(String.valueOf(a));
-       System.out.println(s1.replace(0, 1, "0"));
-       System.out.println("Test");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void IntegerOverFlow(){
-        long l = Integer.MAX_VALUE + 1;
-        System.out.println(l);
-    }
-}
+// /**
+//  * @author lzx
+//  * @date 2023/2/21 14:47
+//  * @description: TODO
+//  */
+// public class Test {
+//     public static void main(String[] args) {
+//
+//     }
+//
+//     @org.junit.jupiter.api.Test
+//     public void agg(){
+//
+//        int a = -2147483648;
+//        StringBuilder s1 = new StringBuilder(String.valueOf(a));
+//        System.out.println(s1.replace(0, 1, "0"));
+//        System.out.println("Test");
+//     }
+//
+//     @org.junit.jupiter.api.Test
+//     public void IntegerOverFlow(){
+//         long l = Integer.MAX_VALUE + 1;
+//         System.out.println(l);
+//     }
+// }
